09688da8c1e554840a63bc139bf91cb6a996ff1f
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2019-01-25 19:51:18 +0100

    Add BeansAmountException exception class

---
+++ CoffeeMaker/src/model/BeansAmountException.java
@@ -0,0 +1,14 @@
+package model;
+
+public class BeansAmountException extends Exception {
+    private double beans;
+
+    public BeansAmountException(double beans) {
+        super(String.format("%f is not the right amount of beans", beans));
+        this.beans = beans;
+    }
+
+    public double getBeans() {
+        return beans;
+    }
+}
