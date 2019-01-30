4783b3a8bd4e61e4dc9f09141adb273e3fd48eab
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2019-01-30 20:00:33 +0100

    Add successful and unsuccessful paths to main method

--- CoffeeMaker/src/main/Main.java
+++ CoffeeMaker/src/main/Main.java
@@ -1,10 +1,60 @@
 package main;
 
+import exceptions.BeansAmountException;
+import exceptions.NoCupsRemainingException;
+import exceptions.StaleCoffeeException;
+import exceptions.WaterException;
+import model.CoffeeMaker;
+
 public class Main {
 
     public static void main(String[] args){
-        // TODO: complete the implementation of Main
-    }
+        // successful path
+        CoffeeMaker coffeeMaker1 = new CoffeeMaker();
 
+        try {
+            coffeeMaker1.brew(2.5, 15);
+        } catch (BeansAmountException|WaterException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            coffeeMaker1.pourCoffee();
+        } catch (NoCupsRemainingException|StaleCoffeeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        coffeeMaker1.setTimeSinceLastBrew(10);
+
+        // unsuccessful path
+        CoffeeMaker coffeeMaker2 = new CoffeeMaker();
+
+        try {
+            coffeeMaker2.brew(1.5, 10);
+        } catch (BeansAmountException|WaterException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            coffeeMaker2.brew(2.5, 15);
+        } catch (BeansAmountException|WaterException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            coffeeMaker2.pourCoffee();
+        } catch (NoCupsRemainingException|StaleCoffeeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        coffeeMaker2.setTimeSinceLastBrew(70);
+
+        try {
+            coffeeMaker2.pourCoffee();
+        } catch (NoCupsRemainingException|StaleCoffeeException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 
 }
