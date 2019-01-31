0f1748c3971a0cb3f76b9feebcd8ed046d45e8ce
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2019-01-31 21:35:41 +0100

    Add CoffeeMaker implementation

--- CoffeeMaker/src/model/CoffeeMaker.java
+++ CoffeeMaker/src/model/CoffeeMaker.java
@@ -9,38 +9,44 @@ import exceptions.*;
  */
 
 public class CoffeeMaker {
+    private int timeSinceLastBrew;
+    private int cupsRemaining;
 
     public CoffeeMaker(){
-        // TODO: complete the implementation of this method
+        timeSinceLastBrew = 0;
+        cupsRemaining = 0;
     }
 
     // getters
     public int getTimeSinceLastBrew() {
-        // TODO: complete the implementation of this method
-        return 0;
+        return timeSinceLastBrew;
     }
     public int getCupsRemaining() {
-        // TODO: complete the implementation of this method
-        return 0;
+        return cupsRemaining;
     }
 
     // EFFECTS: return true if there are coffee cups remaining
     public boolean areCupsRemaining() {
-        // TODO: complete the implementation of this method
-        return false;
+        return cupsRemaining > 0;
     }
 
     //REQUIRES: a non-negative integer
     //EFFECTS: sets time since last brew
     public void setTimeSinceLastBrew(int time) {
-        // TODO: complete the implementation of this method
+        assert(time >= 0);
+        timeSinceLastBrew = time;
     }
 
     //EFFECTS: sets cups remaining to full (20 cups) and time since last brew to 0
     //         throws NotEnoughBeansException if beans less than 2.4, TooManyBeansException
     //         if beans greater than 2.6 and WaterException if water less than or equal to 14.75
     public void brew(double beans, double water) throws BeansAmountException, WaterException {
-        // TODO: complete the implementation of this method
+        if (beans < 2.4) throw new NotEnoughBeansException(beans);
+        if (beans > 2.6) throw new TooManyBeansException(beans);
+        if (water <= 14.75) throw new WaterException(water);
+
+        cupsRemaining = 20;
+        timeSinceLastBrew = 0;
     }
 
     //MODIFIES: this
@@ -48,7 +54,10 @@ public class CoffeeMaker {
     //         throws NoCupsRemainingException if number of cups remaining not greater than 0,
     //         StaleCoffeeException if time since last brew >= 60
     public void pourCoffee() throws NoCupsRemainingException, StaleCoffeeException {
-        // TODO: complete the implementation of this method
+        if (cupsRemaining == 0) throw new NoCupsRemainingException();
+        if (timeSinceLastBrew >= 60) throw new StaleCoffeeException(timeSinceLastBrew);
+
+        cupsRemaining--;
     }
 
 
